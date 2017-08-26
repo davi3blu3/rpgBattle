71e11fec1ea62bb355a45f0bf204dd972db2f82a
Dave Harned <dharned@gmail.com>
2017-08-26 12:08:47 -0500

    cleanup and comment

--- Program.cs
+++ Program.cs
@@ -6,14 +6,24 @@ namespace rpgBattle
     {
         static void Main(string[] args)
         {
+            // Create a new Hero
+            Console.WriteLine("Create a new hero to save the kingdom!");
             Hero hero = new Hero();
-            Console.WriteLine("Create a new hero. Name?");
+
+            // Set Hero's Name
+            Console.WriteLine("Choose a name:");
             hero.Name = Console.ReadLine();
-            Console.WriteLine("Choose a Weapon - (Sword, Axe, or Mace)");
+            
+            // Set Hero's Weapon
+            Console.WriteLine("Choose a Weapon: BroadSword, BattleAxe, Spear)");
             hero.Weapon = Console.ReadLine();
+            
+            // Set Hero's Hit Points
             Random rand = new Random();
             hero.HitPoints = rand.Next(12, 18);
+            
             hero.introduce();
+
         }
     }
 }
